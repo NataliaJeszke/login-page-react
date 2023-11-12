19414581a82a6aefff0dd0b562c47c654bcbf82c
NataliaJeszke <nataliajeszke8@gmail.com>
2023-11-12 20:04:49 +0100

    bootstrap for style

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import "./App.css";
-import Register from "./Register";
-import  {AuthProvider}  from "./context/AuthContext";
+import Register from "./components/Register";
+import { AuthProvider } from "./context/AuthContext";
 
 function App() {
   return (
     <AuthProvider>
-      <>
-        <header>
-          <h1>React Registration Page Project</h1>
-        </header>
-        <Register />
-      </>
+      <div
+        className="d-flex justify-content-center align-items-center"
+        style={{ minHeight: "100vh" }}
+      >
+        <div className="text-center">
+          <header className="mb-5">
+            <h1>Registration Form</h1>
+          </header>
+          <Register />
+        </div>
+      </div>
     </AuthProvider>
   );
 }
---
+++ src/components/Register.jsx
@@ -0,0 +1,79 @@
+import { useState } from "react";
+import { useAuth } from "../context/AuthContext";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import Card from "react-bootstrap/Card";
+
+export default function Register() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const { register } = useAuth();
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    switch (name) {
+      case "email":
+        setEmail(value);
+        break;
+      case "password":
+        setPassword(value);
+        break;
+      default:
+        break;
+    }
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    console.log("Email:", email);
+    console.log("Password:", password);
+
+    register(email, password);
+  };
+  return (
+    <main>
+      <Card style={{ width: "50rem" }}>
+        <Form
+          onSubmit={handleSubmit}
+          className="d-flex flex-column align-items-center justify-content-center"
+        >
+          <Form.Group className="d-flex flex-column align-items-start">
+            <Form.Label className="w-100">
+              Email:
+              <input
+                type="email"
+                name="email"
+                value={email}
+                onChange={handleInputChange}
+                required
+                className="form-control mb-1"
+              />
+            </Form.Label>
+            <Form.Label className="w-100">
+              Password:
+              <input
+                type="password"
+                name="password"
+                value={password}
+                onChange={handleInputChange}
+                required
+                className="form-control mb-1"
+              />
+            </Form.Label>
+            <Button
+              type="submit"
+              style={{ width: "10rem" }}
+              className="mb-3 d-block mx-auto"
+            >
+              Register
+            </Button>
+          </Form.Group>
+        </Form>
+        <div className="text-center">
+          Already have an account? <a href="/login">Login</a>
+        </div>
+      </Card>
+    </main>
+  );
+}
